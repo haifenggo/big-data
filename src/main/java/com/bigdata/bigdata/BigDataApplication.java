777db9775b82b7216795c808a0a12f5dc447056c
haifenggo <haifeng.zh@hotmail.com>
2024-05-08 12:12:31 +0800

    使用websocket

--- src/main/java/com/bigdata/bigdata/BigDataApplication.java
+++ src/main/java/com/bigdata/bigdata/BigDataApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BigDataApplication implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
